e649f8ac66786d8b7fac9ff21e1f7d2bd4a0328a
ST10375560 <st10375560@vcconnect.edu.za>
2025-08-04 14:54:36 +0200

    PhotoShare API

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,26 @@
-console.log('Server is running on port 3000');
+import express from 'express'; 
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// initialise express app
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//middleware to parse JSON bodies
+app.use(express.json());
+
+//Add basic route
+app.get('/', (req, res) => {
+    res.send ('Welcome to the PhotoShare API')
+
+});
+
+//Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
