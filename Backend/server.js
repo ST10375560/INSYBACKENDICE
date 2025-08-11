4ee194769714d99620fdce1177cdfecda039d1e2
ST10375560 <st10375560@vcconnect.edu.za>
2025-08-11 09:51:08 +0200

    database connection

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import fs from 'fs';
 import https from 'https';
+import mongoose from 'mongoose';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 // initialise express app
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = 'mongodb://localhost:27017'
 
 //middleware to parse JSON bodies
 app.use(express.json());
@@ -19,21 +21,33 @@ app.get('/', (req, res) => {
 
 });
 
-//use mkcert-generated certificates for https
-const sslOptions = {
-    key: fs.readFileSync('./certs/localhost-key.pem'),
-    cert: fs.readFileSync('./certs/localhost.pem')
-}
+// use mkcertificates for HTTPs
+const ssloptions = {
+    key: fs.readFileSync('certs/localhost-key.pem'),
+    cert: fs.readFileSync('certs/localhost.pem')
+};
 
-//HTTPS Server
-https.createServer(sslOptions, app).listen(PORT, () => {
+//Add mongodb connection
+mongoose.connect(MONGODB_URI, {
+
+}).then(() => { 
+    console.log('Connected to MongoDB');
+
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
+///HTTPS Server
+https.createServer(ssloptions, app).listen(PORT, () => {
     console.log(`HTTPS Server is running on port ${PORT}`);
-})
+});
+
+
 
 // Start server
- app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
- });
+ //app.listen(PORT, () => {
+    //console.log(`Server is running on port ${PORT}`);
+ //});
 
 
 
